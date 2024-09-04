d96855ebaa9aba47578695c13f9dd65b6f24a4aa
ZanGunawan <zunzee10@gmail.com>
2024-09-04 07:20:17 +0700

    ubah ulamat

--- components/variansatu/V1adress.vue
+++ components/variansatu/V1adress.vue
@@ -2,9 +2,8 @@
     <section id="alamat" class="py-5">
         <div class="container">
             <h4 class="text-center text-success mb-5">Alamat Kami ada di Google 😊</h4>
-            <p class="text-center"><i class="bi bi-geo-alt-fill text-danger me-2"></i>Kp. rt003/001, Desa karang Haur,
-                Kec
-                Pebayuran, Kab Bekasi</p>
+            <p class="text-center"><i class="bi bi-geo-alt-fill text-danger me-2"></i>Kp. Teluk haur rt003/001, Desa
+                karang haur, kec pebayuran, kab bekasi</p>
             <div class="map-wrapper d-flex justify-content-center">
                 <iframe class="w-100"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.6809842108264!2d107.27500591126476!3d-6.173448760471317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6981803e1debdf%3A0x3ef4ef99e0ffb27c!2sAqiqah%20Emak%20Karawang!5e0!3m2!1sid!2sid!4v1724694626842!5m2!1sid!2sid"
